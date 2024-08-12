b8d6b088fdad6a0b72d298c6ebea6b97e78b5b5b
Wojtek Doganowski <wojciech@doganowscy.com>
2024-08-12 22:24:06 +0200

    After 2nd part of tutorial

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,14 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+    on<NumberTriviaEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
