5906904145428fd4f0d0ab931d38fa3915ce97ae
Wojtek Doganowski <wojciech@doganowscy.com>
2024-08-21 20:45:33 +0200

    After part 10 of tutorial

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,12 +1,13 @@
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/entities/number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  NumberTriviaBloc() : super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -6,3 +6,15 @@ abstract class NumberTriviaEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber({required this.numberString});
+
+  @override
+  List<Object> get props => [numberString];
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {}
